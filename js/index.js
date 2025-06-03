ab3b6da8ac19d7b3c8854f6f75223400d9eeb46f
NehaBasak <nehabasak7101996@gmail.com>
2025-06-03 11:15:47 +0530

    update index.html

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const navRight = document.querySelector(".nav-right");
+console.log(navRight);
+const list = document.querySelector(".dash");
+console.log(list);
+list.addEventListener("click", ()=>{
+    console.log(navRight.classList);
+    navRight.classList.toggle("active");
+});
